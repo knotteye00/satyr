7ca1e9023cf2e0ced7c38171f95ba5d92ae6e9e1
knotteye <knotteye@airmail.cc>
2019-10-20 16:10:35 -0500

    Fix a typo in db setup script.

--- install/db_template.sql
+++ install/db_template.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
 	username VARCHAR(25),
 	password_hash BINARY(60),
 	stream_key CHAR(20),
-	record_flag TINYINT,
+	record_flag TINYINT
 );
 CREATE TABLE user_meta(
 	username VARCHAR(25),
