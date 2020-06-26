717d63917c06a2469efcbaae29fb2850a02df97f
knotteye <knotteye@airmail.cc>
2020-06-26 04:52:48 -0500

    Fix typo in registration registration query

--- src/database.ts
+++ src/database.ts
@@ -20,7 +20,7 @@ async function addUser(name: string, password: string){
 	if(dupe[0]) return false;
 	await query('INSERT INTO users (username, password_hash, stream_key, record_flag) VALUES ('+raw.escape(name)+', '+raw.escape(hash)+', '+raw.escape(key)+', 0)');
 	await query('INSERT INTO user_meta (username, title, about, live) VALUES ('+raw.escape(name)+',\'\',\'\',false)');
-	await query('INSERT INTO chat_integration (username, irc, xmpp, twitch, discord) VALUES ('+raw.escape(name)+',\'\',\'\',\'\')');
+	await query('INSERT INTO chat_integration (username, irc, xmpp, twitch, discord) VALUES ('+raw.escape(name)+',\'\',\'\',\'\',\'\')');
 	return true;
 }
 
