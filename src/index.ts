5126bccbc75705273b2de9a7823f032a2a147211
knotteye <knotteye@airmail.cc>
2020-10-17 02:17:10 -0500

    Add configurability to cluster option

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import {init as initHTTP} from "./http";
 import {init as clean} from "./cleanup";
 import {init as initChat} from "./chat";
 import { config } from "./config";
+import { execFile } from "child_process";
 
 async function run() {
 	await initDB();
 	await clean();
 	await initHTTP();
-	await initRTMP();
+	config['rtmp']['cluster'] ? execFile(process.cwd()+'/node_modules/.bin/ts-node' [process.cwd()+'src/cluster.ts']) : await initRTMP();
 	await initChat();
 	console.log(`Satyr v${config['satyr']['version']} ready`);
 }
