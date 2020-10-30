3e073e7f66c39910d3a9336087876603852082e5
knotteye <knotteye@airmail.cc>
2020-10-30 06:25:47 -0500

    Skip compiling templates when running migrations alone

--- src/migrate.ts
+++ src/migrate.ts
@@ -3,6 +3,7 @@ import {init as clean} from "./cleanup";
 import { config } from "./config";
 
 async function run() {
+	process.argv = process.argv.concat(['--skip-compile']);
 	await initDB();
 	await clean();
 }
